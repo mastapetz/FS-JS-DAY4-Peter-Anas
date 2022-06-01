8760646026fa7beacb002826522b6d234d198c02
Peter Aigner <mastapetz@icloud.com>
2022-06-01 14:36:52 +0200

    update basic

--- Peter/01-basic/scripts/script01.js
+++ Peter/01-basic/scripts/script01.js
@@ -1,38 +1,58 @@
-var firstName = document.querySelector("#inputFirstname");
-var lastName = document.querySelector("#inputLastname");
-var age = document.querySelector("#inputAge");
+// var firstName = document.querySelector("#inputFirstname");
+// var lastName = document.querySelector("#inputLastname");
+// var age = document.querySelector("#inputAge");
+// test variables
+// var fName = toString(firstName);
+// var lName = toString(lastName);
 // const outputBox = document.querySelector("#output");
 
-console.log(firstName.value);
-console.log(lastName.value);
-console.log(age.value);
+// console.log(firstName.value);
+// console.log("firstName " + firstName.length + " länge");
+// console.log(lastName.value);
+// console.log("firstName " + lName.length + " länge");
+// console.log(age.value);
 
 const lengthInputCheck = (e) => {
-  console.log(e.target.value);
-  if (e.target.value < 5) {
-    e.target.style.color = "red";
+  // e.preventDefault()
+  var firstName = document.querySelector("#inputFirstname");
+  var lastName = document.querySelector("#inputLastname");
+  var age = document.querySelector("#inputAge");
+  if (firstName.value.length < 5) {
+    firstName.style.color = "red";
+    document.querySelector(
+      "#output"
+    ).innerHTML += `<span class="red">Firstname: ${firstName.value}</span><br>`;
   } else {
-    e.target.style.color = "green";
+    firstName.style.color = "green";
+    document.querySelector(
+      "#output"
+    ).innerHTML += `<span class="green">Firstname: ${firstName.value}</span><br>`;
   }
+  if (lastName.value.length < 5) {
+    lastName.style.color = "red";
+    document.querySelector(
+      "#output"
+    ).innerHTML += `<span class="red">Lastname: ${lastName.value}</span><br>`;
+  } else {
+    lastName.style.color = "green";
+    document.querySelector(
+      "#output"
+    ).innerHTML += `<span class="green">Lastname: ${lastName.value}</span><br>`;
+  }
+
+  document.querySelector("#output").innerHTML += `Age: ${age.value}`;
 };
 
 // checks firstnames length when entered
 document
-  .querySelector("#inputFirstname")
+  .querySelector("#checkLength")
   .addEventListener("click", lengthInputCheck);
 
 // checks lastnames length when entered
-document
-  .querySelector("#inputLastname")
-  .addEventListener("click", lengthInputCheck);
+// document
+//   .querySelector("#inputLastname")
+//   .addEventListener("focusout", lengthInputCheck);
 
 // prints the content of the variable into the div #output
 // += needed for all values to be  displayed or they would be overwritten otherwise
 // the <br> give the linebreak in the DOM
-document.querySelector(
-  "#output"
-).innerHTML = `Firstname: ${firstName.value}<br>`;
-document.querySelector(
-  "#output"
-).innerHTML += `Lastname: ${lastName.value}<br>`;
-document.querySelector("#output").innerHTML += `Age: ${age.value}`;
