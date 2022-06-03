f286fddfa48fcb3172bb545cca3442d5aa78659d
Anas Alhealy <Anas2007jalil@yahoo.com>
2022-06-03 11:49:35 +0200

    starting

---
+++ Anas/1.basic/scripts/script01.js
@@ -0,0 +1,49 @@
+// document.getElementById("counters").innerHTML = 4
+
+// var myAge = (`im ${28} years old`)
+// console.log(myAge);
+
+// firstEX
+
+// let firstPass = 5
+// let secondPass = 7
+// let count = firstPass + secondPass
+// console.log(count);
+
+// secondEX
+
+// var myAge = 28
+// var humanDogRatio = 14
+// let myDogAge = myAge * humanDogRatio
+
+// console.log(myDogAge);
+
+// thirdEX
+
+// let bounsPoints = 50
+// console.log(bounsPoints)
+
+// bounsPoints = bounsPoints + 50
+// console.log(bounsPoints);
+
+// bounsPoints = bounsPoints - 75
+// console.log(bounsPoints);
+
+// bounsPoints = bounsPoints + 45
+// console.log(bounsPoints);
+
+// fourthEX 
+
+// let lap1 = 34
+// let lap2 = 33
+// let lap3 = 36
+
+// function allLabs() {
+//     let totalLabs = lap1 + lap2 + lap3
+//     console.log(totalLabs);
+// }
+// allLabs();
+
+
+
+// fifthEX
